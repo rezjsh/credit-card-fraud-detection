31d732855e18881700b42ed84605f64589d8803e
rezjsh <149146755+rezjsh@users.noreply.github.com>
2026-06-30 10:02:41 +0330

    Add Data EDA pipeline integration in main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from credit_card_fraud_detection.config.configuration import ConfigurationManager
 from credit_card_fraud_detection.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from credit_card_fraud_detection.pipeline.stage_02_data_eda import DataEDAPipeline
+from credit_card_fraud_detection.pipeline.stage_03_data_validation import DataValidationTrainingPipeline
 from credit_card_fraud_detection.utils.logging_setup import logger
 
 
@@ -17,6 +19,21 @@ def main():
         data_ingestion_pipeline = DataIngestionPipeline(config_manager)
         data_ingestion_pipeline.run_pipeline()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n")
+
+        #   # --- Stage 2: Data EDA ---
+        # STAGE_NAME = "Stage 02: Data EDA"
+        # logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        # eda_pipeline = DataEDAPipeline(config_manager)
+        # eda_pipeline.run_pipeline()
+        # logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n")
+
+        # --- Stage 3: Data Validation ---
+        STAGE_NAME = "Stage 03: Data Validation"
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        validation_pipeline = DataValidationTrainingPipeline()
+        validation_pipeline.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n")
+
 
    
     except Exception as e:
